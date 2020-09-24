e12581eee59e4aeab0da2d32a52ff25d4ad38f4d
BisiDavi <oludavidconnect@gmail.com>
2020-09-24 11:26:57 +0100

    .

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,12 +16,16 @@
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="{{ asset('js/jssor.slider-28.0.0.min.js') }}"></script>
+  <link rel="stylesheet" href="css/slider.css">
   @yield('link')
 </head>
 
 <body>
 
   @include('partials.nav')
+
+  @include('partials.slider')
 
   @yield('content')
 
