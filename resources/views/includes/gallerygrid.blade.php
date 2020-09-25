40daeb67d38a4209cc36a364e5158d159c2efd0c
BisiDavi <oludavidconnect@gmail.com>
2020-09-25 09:51:36 +0100

    .

---
+++ resources/views/includes/gallerygrid.blade.php
@@ -0,0 +1,26 @@
+<div class="gallery-grid">
+  <div class="image-container item1">
+    <a class="img-1" href="images/gallery-1.jpg">
+      <img src="images/gallery-1.jpg" alt=""></a>
+  </div>
+  <div class="image-container item2">
+    <a class="img-1" href="images/gallery-2.jpg">
+      <img src="images/gallery-2.jpg" alt=""></a>
+  </div>
+  <div class="image-container item3">
+    <a class="img-1" href="images/gallery-3.jpg">
+      <img src="images/gallery-3.jpg" alt=""></a>
+  </div>
+  <div class="image-container item4">
+    <a class="img-1" href="images/gallery-4.jpg">
+      <img src="images/gallery-4.jpg" alt=""></a>
+  </div>
+  <div class="image-container item5">
+    <a class="img-1" href="images/gallery-5.jpg">
+      <img src="images/gallery-5.jpg" alt=""></a>
+  </div>
+  <div class="image-container item6">
+    <a class="img-1" href="images/gallery-6.jpg">
+      <img src="images/gallery-6.jpg" alt=""></a>
+  </div>
+</div>
