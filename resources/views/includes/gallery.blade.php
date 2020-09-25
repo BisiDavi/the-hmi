814340444f555d7baecb2916b0b6105fce66d019
BisiDavi <oludavidconnect@gmail.com>
2020-09-25 10:33:37 +0100

    .

--- resources/views/includes/gallery.blade.php
+++ resources/views/includes/gallery.blade.php
@@ -6,26 +6,62 @@
     </div>
     <div class="gallery-grid">
       <div class="image-container item1">
+        <div class="gallery-overlay">
+          <a href="#">
+            <i class="fa fa-pinterest" aria-hidden="true"></i>
+            <p>Save</p>
+          </a>
+        </div>
         <a class="img-1" href="images/gallery-1.jpg">
           <img src="images/gallery-1.jpg" alt=""></a>
       </div>
       <div class="image-container item2">
+        <div class="gallery-overlay">
+          <a href="#">
+            <i class="fa fa-pinterest" aria-hidden="true"></i>
+            <p>Save</p>
+          </a>
+        </div>
         <a class="img-1" href="images/gallery-2.jpg">
           <img src="images/gallery-2.jpg" alt=""></a>
       </div>
       <div class="image-container item3">
+        <div class="gallery-overlay">
+          <a href="#">
+            <i class="fa fa-pinterest" aria-hidden="true"></i>
+            <p>Save</p>
+          </a>
+        </div>
         <a class="img-1" href="images/gallery-3.jpg">
           <img src="images/gallery-3.jpg" alt=""></a>
       </div>
       <div class="image-container item4">
+        <div class="gallery-overlay">
+          <a href="#">
+            <i class="fa fa-pinterest" aria-hidden="true"></i>
+            <p>Save</p>
+          </a>
+        </div>
         <a class="img-1" href="images/gallery-4.jpg">
           <img src="images/gallery-4.jpg" alt=""></a>
       </div>
       <div class="image-container item5">
+        <div class="gallery-overlay">
+          <a href="#">
+            <i class="fa fa-pinterest" aria-hidden="true"></i>
+            <p>Save</p>
+          </a>
+        </div>
         <a class="img-1" href="images/gallery-5.jpg">
           <img src="images/gallery-5.jpg" alt=""></a>
       </div>
       <div class="image-container item6">
+        <div class="gallery-overlay">
+          <a href="#">
+            <i class="fa fa-pinterest" aria-hidden="true"></i>
+            <p>Save</p>
+          </a>
+        </div>
         <a class="img-1" href="images/gallery-6.jpg">
           <img src="images/gallery-6.jpg" alt=""></a>
       </div>
